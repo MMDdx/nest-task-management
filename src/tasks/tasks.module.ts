1fc7eb15c52b96fd04f3889ae95dafc1d4b3dfa2
Mohammad Kabiri <mohamadnt8@yahoo.com>
2026-08-03 18:34:49 +0330

    added task model and service

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
-@Module({})
-export class TasksModule {}
+@Module({
+  controllers: [TasksController],
+  providers: [TasksService]
+})
+export class TasksModule {
+
+}
