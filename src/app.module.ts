2ebf699ff3ab39665ca1a68119299bece4481269
Mohammad Kabiri <mohamadnt8@yahoo.com>
2026-08-09 17:34:20 +0330

    added typeorm but .env must be implemented in future

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule ,
+    TypeOrmModule.forRoot({
+    type : 'postgres',
+    host : 'localhost',
+    port: 5432,
+    username : 'postgres',
+    password: '123456',
+      autoLoadEntities : true,
+      synchronize : true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
