b73d12292ee36d101e198a49cbcb9939ae35b872
Mohammad Kabiri <mohamadnt8@yahoo.com>
2026-08-16 18:19:08 +0330

    updated auth dto and user entity

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -15,8 +15,17 @@ export class UsersRepository {
     const { username, password } = AuthCredentialsDto;
 
     const user = this.repository.create({ username, password });
-
-    await this.repository.save(user)
+    try {
+      await this.repository.save(user)
+    }
+    catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('username already exists');
+      } else{
+        throw new InternalServerErrorException()
+      }
+    }
   }
 
 }
